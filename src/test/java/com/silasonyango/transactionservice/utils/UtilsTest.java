f3fad5ec2b4e56e30c0c0fd1bbeaa0803e494db4
Silas Onyango <silas.onyango93@gmail.com>
2020-11-26 23:38:15 +0300

    feat(tests): setup test suite

---
+++ src/test/java/com/silasonyango/transactionservice/utils/UtilsTest.java
@@ -0,0 +1,15 @@
+package com.silasonyango.transactionservice.utils;
+
+import com.silasonyango.transactionservice.utility_classes.UtilityClass;
+import com.silasonyango.transactionservice.utility_classes.Utils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtilsTest {
+    @Test
+    public void toCamelCase_works_properly() {
+        assertEquals("Silas Onyango", Utils.toCamelCase("SILAS ONYANGO"));
+    }
+
+}
