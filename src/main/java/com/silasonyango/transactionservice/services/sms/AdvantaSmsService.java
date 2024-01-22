53109437513dd2fee0d86f43c293f44ef6e4c1c7
silasonyango93 <silas.onyango93@gmail.com>
2024-01-22 16:26:58 +0300

    complete advanta sms service setup

--- src/main/java/com/silasonyango/transactionservice/services/sms/AdvantaSmsService.java
+++ src/main/java/com/silasonyango/transactionservice/services/sms/AdvantaSmsService.java
@@ -1,5 +1,6 @@
 package com.silasonyango.transactionservice.services.sms;
 
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -18,10 +19,35 @@ public class AdvantaSmsService {
   @Autowired
   OkHttpClient okHttpClient;
 
+
   public boolean sendSms(String recipientPhoneNo, String textMessage) {
+    Request request = new Request.Builder()
+        .url(String.format(
+            "https://quicksms.advantasms.com/api/services/sendsms/?apikey=de8d4d1e223c0d21e685eee3cf0162b6"
+                + "&partnerID=9427&message=%s&shortcode=WAONDO-SEC&mobile=%s", textMessage,
+            recipientPhoneNo))
+        .build();
+
+    try (Response response = okHttpClient.newCall(request).execute()) {
+      log.info(
+          String.format("sms status for recipient %s is: status code -> %s; response body -> %s",
+              recipientPhoneNo, response.code(),
+              response.body() != null ? response.body().toString() : null));
+      return true;
+    } catch (Exception e) {
+      log.info(String.format("sms error message for recipient %s is -> %s", recipientPhoneNo,
+          e.getMessage()));
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public boolean postSendSms(String recipientPhoneNo, String textMessage) {
+    log.info("Initiate sms send");
+    log.info(System.getenv("ADVANTA_API_KEY"));
     String url = "https://quicksms.advantasms.com/api/services/sendsms/";
     AdvantaSmsServiceModel payload = new AdvantaSmsServiceModel(
-        System.getenv("ADVANTA_API_KEY"),
+        "de8d4d1e223c0d21e685eee3cf0162b6",
         "9427",
         textMessage,
         "WAONDO-SEC",
@@ -30,6 +56,7 @@ public class AdvantaSmsService {
 
     RequestBody requestBody = RequestBody.create(payload.toString(),
         MediaType.parse("application/json"));
+    log.info(requestBody.toString());
     Request request = new Request.Builder()
         .url(url)
         .post(requestBody)
@@ -37,7 +64,8 @@ public class AdvantaSmsService {
     try (Response response = okHttpClient.newCall(request).execute()) {
       log.info(
           String.format("sms status for recipient %s is: status code -> %s; response body -> %s",
-              recipientPhoneNo, response.code(), response.body()));
+              recipientPhoneNo, response.code(),
+              response.body() != null ? response.body().toString() : null));
       return response.isSuccessful();
     } catch (Exception e) {
       log.info(String.format("sms error message for recipient %s is -> %s", recipientPhoneNo,
